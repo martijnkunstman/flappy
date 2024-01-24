b3871c0b5a26e6ad064b2b6170ee009852fa4282
Martijn Kunstman <martijn@kunstman.nl>
2024-01-24 14:08:31 +0100

    debug

--- device-orientation-controls.js
+++ device-orientation-controls.js
@@ -1,11 +1,16 @@
 let debug = document.getElementById("debug");
 debug.innerHTML = "Device Orientation API is not supported by your browser.";
 
-const acl = new Accelerometer({ frequency: 60 });
+const acl = new Accelerometer({ frequency: 10 });
 acl.addEventListener("reading", () => {
-    debug.innerHTML = "X: "+acl.x;
-    debug.innerHTML += "Y: "+acl.y;
-    debug.innerHTML += "Z: "+acl.z;  
+    
+   let x = (Math.round(acl.x * 100) / 100).toFixed(2);
+   let y = (Math.round(acl.y * 100) / 100).toFixed(2);
+   let z = (Math.round(acl.z * 100) / 100).toFixed(2);
+    
+    debug.innerHTML = "X: "+x+"<br>";
+    debug.innerHTML += "Y: "+y+"<br>";
+    debug.innerHTML += "Z: "+z+"<br>";  
 });
 
 acl.start();
