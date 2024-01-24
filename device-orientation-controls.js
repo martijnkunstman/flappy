b61093b159daa858283ec1e289edf84b7ee49ac6
Martijn Kunstman <martijn@kunstman.nl>
2024-01-24 23:05:21 +0100

    android

--- device-orientation-controls.js
+++ device-orientation-controls.js
@@ -4,36 +4,62 @@ debug.innerHTML = "Device Orientation API is not supported by your browser.";
 let jumpvalue = 0;
 let jump = false;
 
-function requestOrientationPermission(){
-    console.log('requesting orientation permission')
+function requestOrientationPermission() {
+  console.log("requesting orientation permission");
+
+  if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+    console.log("This is an iOS device.");
+
     DeviceOrientationEvent.requestPermission()
-    .then(response => {
-        if (response == 'granted') {
-            window.addEventListener('deviceorientation', (e) => {
-              const acl = new Accelerometer({ frequency: 10 });
-              acl.addEventListener("reading", () => {
-                let x = (Math.round(acl.x * 100) / 100).toFixed(2);
-              
-                if (!jump) {
-                  if (x > 2 || x < -2) {
-                    jumpvalue = 1;
-                    jump = true;
-                    bird_dy = -7.6;
-                  }
+      .then((response) => {
+        if (response == "granted") {
+          window.addEventListener("deviceorientation", (e) => {
+            const acl = new Accelerometer({ frequency: 10 });
+            acl.addEventListener("reading", () => {
+              let x = (Math.round(acl.x * 100) / 100).toFixed(2);
+
+              if (!jump) {
+                if (x > 2 || x < -2) {
+                  jumpvalue = 1;
+                  jump = true;
+                  bird_dy = -7.6;
                 }
-                if (x < 1 && x > -1) {
-                  jumpvalue = 0;
-                  jump = false;
-                }
-              
-                debug.innerHTML = jumpvalue + " - "+x;
-              });
-              acl.start();
-            })
+              }
+              if (x < 1 && x > -1) {
+                jumpvalue = 0;
+                jump = false;
+              }
+
+              debug.innerHTML = jumpvalue + " - " + x;
+            });
+            acl.start();
+          });
         }
-    })
-    .catch(console.error)
+      })
+      .catch(console.error);
+  } else {
+    console.log("This is not an iOS device!");
+    window.addEventListener("deviceorientation", (e) => {
+      const acl = new Accelerometer({ frequency: 10 });
+      acl.addEventListener("reading", () => {
+        let x = (Math.round(acl.x * 100) / 100).toFixed(2);
+
+        if (!jump) {
+          if (x > 2 || x < -2) {
+            jumpvalue = 1;
+            jump = true;
+            bird_dy = -7.6;
+          }
+        }
+        if (x < 1 && x > -1) {
+          jumpvalue = 0;
+          jump = false;
+        }
+
+        debug.innerHTML = jumpvalue + " - " + x;
+      });
+      acl.start();
+    });
+    
+  }
 }
-
-
-
