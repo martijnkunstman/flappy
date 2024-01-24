ef31f247f9be3352a6d3d235995be569b549d3a3
Martijn Kunstman <martijn@kunstman.nl>
2024-01-24 21:46:58 +0100

    ios

--- device-orientation-controls.js
+++ device-orientation-controls.js
@@ -1,26 +1,40 @@
 let debug = document.getElementById("debug");
 debug.innerHTML = "Device Orientation API is not supported by your browser.";
-const acl = new Accelerometer({ frequency: 10 });
+
 
 let jumpvalue = 0;
 let jump = false;
 
-acl.addEventListener("reading", () => {
-  let x = (Math.round(acl.x * 100) / 100).toFixed(2);
+function requestOrientationPermission(){
+    console.log('requesting orientation permission')
+    DeviceOrientationEvent.requestPermission()
+    .then(response => {
+        if (response == 'granted') {
+            window.addEventListener('deviceorientation', (e) => {
+              const acl = new Accelerometer({ frequency: 10 });
+              acl.addEventListener("reading", () => {
+                let x = (Math.round(acl.x * 100) / 100).toFixed(2);
+              
+                if (!jump) {
+                  if (x > 2 || x < -2) {
+                    jumpvalue = 1;
+                    jump = true;
+                    bird_dy = -7.6;
+                  }
+                }
+                if (x < 1 && x > -1) {
+                  jumpvalue = 0;
+                  jump = false;
+                }
+              
+                debug.innerHTML = jumpvalue + " - "+x;
+              });
+              acl.start();
+            })
+        }
+    })
+    .catch(console.error)
+}
 
-  if (!jump) {
-    if (x > 2 || x < -2) {
-      jumpvalue = 1;
-      jump = true;
-      bird_dy = -7.6;
-    }
-  }
-  if (x < 1 && x > -1) {
-    jumpvalue = 0;
-    jump = false;
-  }
 
-  debug.innerHTML = jumpvalue + " - "+x;
-});
 
-acl.start();
